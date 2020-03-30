739f664efe11ac2be48df5bab993131d9185219a
Vincent Schlosser <s.vincent@live.fr>
2020-03-30 13:52:20 +0200

    TeamCard component

---
+++ src/components/WaitingRoom/TeamCard/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyledTeamWrapper, StyledTeamContainer, StyledTitle, StyledRoleTitle } from './styled';
+import { StyledPseudoWrapper, StyledPseudo } from '../styled';
+import Button from '../../Button';
+
+const TeamCard = ({
+    team,
+    onClickSpy,
+    onClickAgent,
+    players,
+}) => {
+
+  return (
+    <StyledTeamWrapper>
+      <StyledTitle team={team}>{team} Team</StyledTitle>
+      <StyledTeamContainer onClick={onClickSpy} text={'Go spy'}>
+        <StyledRoleTitle>{team} spy</StyledRoleTitle>
+          <StyledPseudoWrapper>
+            {players.map( player => (
+              (player.role === "BS" || player.role === "RS") &&
+              <StyledPseudo>{player.pseudo}</StyledPseudo>
+            ))}
+          </StyledPseudoWrapper>
+      </StyledTeamContainer>
+      <StyledTeamContainer onClick={onClickAgent} text={'Go spy'}>
+        <StyledRoleTitle>{team} agents</StyledRoleTitle>
+
+          <StyledPseudoWrapper>
+            {players.map( player => (
+              (player.role === "BA" || player.role === "RA") &&
+              <StyledPseudo>{player.pseudo}</StyledPseudo>
+            ))}
+          </StyledPseudoWrapper>
+      </StyledTeamContainer>
+    </StyledTeamWrapper>
+  )
+};
+
+export default TeamCard;
---
+++ src/components/WaitingRoom/TeamCard/styled.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import { color } from '../../../utils/branding';
+
+const StyledTeamWrapper = styled.section`
+  width: 33%;
+  text-align: center;
+  align-items: center;
+`;
+
+const StyledTitle = styled.h3`
+  color: ${({team}) => team === 'Blue' ? color.blue : color.red};
+  font-size: 26px;
+`;
+
+const StyledTeamContainer = styled.div`
+  border-radius: 12px;
+  box-shadow: 9px 9px 16px #A3B1C6, -9px -9px 16px #FFFFFF;
+  padding-top: 2px;
+  padding-bottom: 16px;
+  margin: 0 auto 24px;
+  min-height: 250px;
+  max-width: 400px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledRoleTitle = styled.h4`
+  font-size: 22px;
+`;
+
+
+export {
+    StyledTeamWrapper,
+    StyledTeamContainer,
+    StyledTitle,
+    StyledRoleTitle,
+};
